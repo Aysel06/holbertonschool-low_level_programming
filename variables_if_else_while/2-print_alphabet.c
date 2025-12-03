419aa6d0c9fc9d7829ae23b833e2a6a71d896f27
Aysel06 <mammedovaaysel0@gmail.com>
2025-12-03 14:10:34 +0000

    2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int c;
-    for (c = 'a'; c <= 'z'; c++)
-        putchar(c);
-    putchar('\n');
-    return 0;
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }
