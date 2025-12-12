c44e5db9dfb27cad6e815c030d613d9e9101c8db
Aysel06 <mammedovaaysel0.com>
2025-12-12 16:01:36 +0000

    3

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 void reset_to_98(int *n);
 int _strlen(char *s);
 void _puts(char *str);
+void swap_int(int *a, int *b);
+int _putchar(char c);
+
+
 #endif /*MAIN.H*/
