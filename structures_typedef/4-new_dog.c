0dc547a9bc4aa71401211e97dc43021a62d6f404
Aysel06 <mammedovaaysel0@gmail.com>
2026-01-07 14:29:41 +0000

    asel

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,6 +13,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	int len_name, len_owner;
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
@@ -21,20 +22,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (d == NULL)
 		return (NULL);
 
-	d->name = strdup(name);
+	len_name = strlen(name);
+	d->name = malloc(len_name + 1);
 	if (d->name == NULL)
 	{
 		free(d);
 		return (NULL);
 	}
+	strcpy(d->name, name);
 
-	d->owner = strdup(owner);
+	len_owner = strlen(owner);
+	d->owner = malloc(len_owner + 1);
 	if (d->owner == NULL)
 	{
 		free(d->name);
 		free(d);
 		return (NULL);
 	}
+	strcpy(d->owner, owner);
 
 	d->age = age;
 
